28da0cfd91c32195be71b6058ba2fe0ce19916ab
Victor Reipur <victor.reipur@gmail.com>
2023-03-02 13:10:53 +0000

    configure mysql

--- find_artek/settings.py
+++ find_artek/settings.py
@@ -76,8 +76,17 @@ WSGI_APPLICATION = 'find_artek.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'sql_mode': 'traditional',
+        },
+        'NAME': 'testuser_find_artek_v1_0_0',
+        'USER': 'testuser',
+        'PASSWORD': 'notSecureChangeMe',
+        'HOST': 'database-service',
+        'PORT': '3306',
     }
 }
 
