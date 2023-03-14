5ef62231b34e67937b79d7030d17e04fc2e7ab62
Victor Reipur <victor.reipur@gmail.com>
2023-03-14 10:54:23 +0000

    removing world - tutorial app

--- find_artek/settings.py
+++ find_artek/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'django.contrib.gis',
-    'world',
-    'publications'
+    'publications',
 ]
 
 MIDDLEWARE = [
