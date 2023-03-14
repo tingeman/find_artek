e7e9cbf0123dfc5783fe2259b8639936ef5b0ea9
Victor Reipur <victor.reipur@gmail.com>
2023-03-14 10:52:19 +0000

    admin panel

--- publications/admin.py
+++ publications/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Publication, Feature
 
 # Register your models here.
+admin.site.register(Publication, admin.ModelAdmin)
+# admin.site.register(Author, admin.ModelAdmin)
+# admin.site.register(Publisher, admin.ModelAdmin)
+admin.site.register(Feature, admin.ModelAdmin)
