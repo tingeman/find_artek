5d23f502424656d2201297a8ec8467bf2bd8d5a4
Victor Reipur <victor.reipur@gmail.com>
2023-08-06 14:40:48 +0000

    ignore app/venv

--- app/venv/lib/python3.10/site-packages/django_cas_ng/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CASConfig(AppConfig):
-    default_auto_field = 'django.db.models.AutoField'
-    name = 'django_cas_ng'
--- app/venv/lib/python3.10/site-packages/django_cas_ng/signals.py
+++
@@ -1,8 +0,0 @@
-from django import dispatch
-
-# Arguments passed to receiver functions are documented at
-# https://djangocas.dev/docs/latest/signals.html
-
-cas_user_authenticated = dispatch.Signal()
-
-cas_user_logout = dispatch.Signal()
