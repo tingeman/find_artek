950252d15504d9158f2efc5db14e2ce429bd1c8e
Victor Reipur <victor.reipur@gmail.com>
2023-08-14 13:58:19 +0000

    minor changes

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+
+
 # cd ./app and remove venv if it exits, then rebuild venv, activate it, and install requirements
-cd ./app && rm -rf venv && python3 -m venv venv && source venv/bin/activate && pip install --upgrade pip && pip install -r requirements.txt
+pwd && cd app && ls && rm -rf venv && python3 -m venv venv && source venv/bin/activate && pip install --upgrade pip && pip install -r requirements.txt
 
 git config --global user.email "victor.reipur@gmail.com"
 git config --global user.name "Victor Reipur"
 git config --global --add safe.directory /usr/src/find_artek
+
+# pwd
