89adc9239a08c323aefcdaeb2b85d74ffb6e0850
Victor Reipur <victor.reipur@gmail.com>
2023-11-22 20:24:01 +0000

    frontpage now background loads map

--- app-main/publications/static/publications/js/frontpage.js
+++ app-main/publications/static/publications/js/frontpage.js
@@ -1,6 +1,34 @@
+// ============
+// Initialization
+// ============
+document.addEventListener("DOMContentLoaded", function (event) {
+    // This block will run when the DOM is loaded.
+    main().catch(error => {
+        console.error("Error initializing map or fetching feature data: ", error);
+        // handle error, for example by showing an error message to the user
+    });
+});
 
-x = 3542;
-x = 2 * x;
-x = x.toString();
-console.log(x);
-console.log("frontpage.js is loaded.");
+
+
+
+
+// // ============
+// // Main
+// // ============
+async function main() {
+
+    const myMapClass = new MyMapClass();
+
+    try {
+
+        // load and cache feature data in background
+        const featureData = await myMapClass.getFeatureData()
+
+
+
+    } catch (error) {
+        console.error("Error: ", error);
+    }
+
+}
