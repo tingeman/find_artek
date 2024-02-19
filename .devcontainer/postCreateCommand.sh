4521c0492331e424fa3a0fb9d77c5bcf950acd68
Victor Reipur <victor.reipur@gmail.com>
2024-02-19 20:23:13 +0000

    speed commit

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,7 +2,9 @@
 
 
 # cd ./app and remove venv if it exits, then rebuild venv, activate it, and install requirements
-pwd && cd app-main && ls && rm -rf venv && python3 -m venv venv && source venv/bin/activate && pip install --upgrade pip && pip install -r requirements.txt
+# pwd && cd app-main && ls && rm -rf venv && python3 -m venv venv && source venv/bin/activate && pip install --upgrade pip && pip install -r requirements.txt
+
+cd app-main && source venv/bin/activate
 
 git config --global user.email "victor.reipur@gmail.com"
 git config --global user.name "Victor Reipur"
