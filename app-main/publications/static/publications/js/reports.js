89a8ef52b998fbce8f4736e92f2629a691449575
tingeman <thin@dtu.dk>
2024-12-10 10:21:34 +0100

    Added the JS_URL_PREFIX to link generation in javascript

--- app-main/publications/static/publications/js/reports.js
+++ app-main/publications/static/publications/js/reports.js
@@ -36,7 +36,7 @@ async function main() {
     const myReportsClass = new MyReportsClass(
         reportsTableList,
         totalReportsNumber,
-        url = JS_URL_PREFIX + JS_URL_PREFIX + '/api/report/'
+        url = JS_URL_PREFIX + '/api/report/'
         );
 
 
