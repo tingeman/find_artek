b152dad2fdb1acc9225043eb8955ad29bc2ff1bd
tingeman <thin@dtu.dk>
2024-12-10 17:11:25 +0100

    Map now uses crs 3575 North Pole LAEA

--- app-main/publications/static/publications/js/class/my-map-class.js
+++ app-main/publications/static/publications/js/class/my-map-class.js
@@ -24,31 +24,73 @@ class MyMapClass {
 
         // if lat, lng or zoom are not defined, use default values
         if (!lat || !lng || !zoom) {
-          lat = 74.86;
-          lng = -44.60;
+          lat = 70.00;
+          lng = -44.00; // Fixed syntax error: removed extra dot
           zoom = 4.00;
         }
 
-        // ================= this part is to (attempt) retrive a location, that is passed in the url ================= //
-        // ================= http://localhost:8080/publications/map/?lat=66.99&lng=-53.22&zoom=14.00 ================= //
+        // // ================= this part is to (attempt) retrive a location, that is passed in the url ================= //
+        // // ================= http://localhost:8080/publications/map/?lat=66.99&lng=-53.22&zoom=14.00 ================= //
 
 
 
-        // Create the map, set the view to the lat, lng, and zoom
-        // I think lat, lng, and zoom will be what you see in the map.
-        const map = L.map('map').setView([lat, lng], zoom);
-        const url = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
-        const options = {
-          maxZoom: 29,
-          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">'
-        };
-        L.tileLayer(url, options).addTo(map);
+        // // Create the map, set the view to the lat, lng, and zoom
+        // // I think lat, lng, and zoom will be what you see in the map.
+        // const map = L.map('map').setView([lat, lng], zoom);
+        // const url = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
+        // const options = {
+        //   maxZoom: 29,
+        //   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">'
+        // };
+        // L.tileLayer(url, options).addTo(map);
+
+        // // ================= this part is that sets a location, that is passed in the url             ================= //
+        // // ================= http://localhost:8080/publications/map/?lat=66.99&lng=-53.22&zoom=14.00  ================= //
+        // // it sort of a side effect, that is happening here, which i am not pitucularly big fan of. but it is my implementation so fare.
 
 
-        // ================= this part is that sets a location, that is passed in the url             ================= //
-        // ================= http://localhost:8080/publications/map/?lat=66.99&lng=-53.22&zoom=14.00  ================= //
-        // it sort of a side effect, that is happening here, which i am not pitucularly big fan of. but it is my implementation so fare.
+        // ================= THIN attempt at better map projection ================= //
+        var pixel_ratio = parseInt(window.devicePixelRatio) || 1;
+        var max_zoom = 16;
+        var tile_size = 512;
+        var extent = Math.sqrt(2) * 6371007.2;
+        var resolutions = Array(max_zoom + 1).fill().map((_, i) => ( extent / tile_size / Math.pow(2, i-1) ));
 
+        console.log('Debug information:')
+        console.log(proj4); // Should log the Proj4 object
+        console.log(L.Proj); // Should log the Proj4Leaflet namespace
+
+        var crs = new L.Proj.CRS(
+          'EPSG:3575',
+          '+proj=laea +lat_0=90 +lon_0=10 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs',
+          {
+              origin: [-extent, extent],
+              projectedBounds: L.bounds(L.point(-extent, extent), L.point(extent, -extent)),
+              resolutions: resolutions
+          }
+        );
+
+        var map = L.map('map', {
+          crs: crs,
+          rotate: true,
+        }).setView([lat, lng], zoom);
+
+        try {
+          //https://gibs.earthdata.nasa.gov/wmts/epsg{*EPSG:Code*}/best/{*LayerIdentifier*}/default/{*Time*}/{*TileMatrixSet*}/{*ZoomLevel*}/{*TileRow*}/{*TileCol*}.png
+          //https://gibs.earthdata.nasa.gov/wmts/epsg3413/best/Reference_Features_15m/default/2014-04-09/GoogleMapsCompatible_Level6/{z}/{x}/{y}.png
+          L.tileLayer('https://tile.gbif.org/3575/omt/{z}/{x}/{y}@{r}x.png?style=osm-light'.replace('{r}', pixel_ratio), {
+              tileSize: tile_size,
+              minZoom: 1,
+              maxZoom: 16
+          }).addTo(map);
+        }
+        catch (err) {
+            console.error(err);
+        }
+        
+        map.setBearing(-54);
+        // ================= END of THIN attempt at better map projection ================= //
+        
         map.on('moveend zoomend', function () {
           const center = map.getCenter();
           const zoom = map.getZoom();
@@ -164,11 +206,6 @@ class MyMapClass {
         map.fitBounds(bounds);
       }
     }
-
-
-
-
-
   }
 
 
