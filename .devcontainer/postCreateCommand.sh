8ec508761a2401ddc3ea597e0ecaec6725b4db9b
tingeman <thin@dtu.dk>
2024-12-12 01:22:41 +0100

    Fixed the .devcontainer setup

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # cd ./app and remove venv if it exits, then rebuild venv, activate it, and install requirements
 # pwd && cd app-main && ls && rm -rf venv && python3 -m venv venv && source venv/bin/activate && pip install --upgrade pip && pip install -r requirements.txt
 
